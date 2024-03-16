ae2bd68b40defa16dfe442535be9a9b8e7c9d97d
Pedro Tibúrcio <pedrotiburcio10@gmail.com>
2024-03-15 21:54:24 -0300

    adding docker-compose and makefile

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -138,9 +138,3 @@ REST_FRAMEWORK = {
 }
 
 INTERNAL_IPS = ['127.0.0.1', ]
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-DEBUG = int(os.environ.get("DEBUG", default=0))
-
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
