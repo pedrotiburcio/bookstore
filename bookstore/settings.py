74d31ad72a1bdf9665011a0ec608a83f1ab62251
pedrotiburcio <pedrotiburcio10@gmail.com>
2024-08-01 11:59:32 -0300

    adding whitenoise

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -155,3 +156,7 @@ LOGGING = {
         "level": "WARNING",
     },
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
